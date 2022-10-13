c8fb45096adfabdfbe27fb2e898de96162f3837c
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-13 11:57:47 +0100

    initial commit

---
+++ src/main/java/ma/usmba/springbankbackend/SpringBankBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.springbankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBankBackendApplication.class, args);
+    }
+
+}
